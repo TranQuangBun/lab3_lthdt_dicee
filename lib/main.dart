572777b925b2645a56c3842ba9ec41c83bcd7cb3
TranQuang <quangt.21it@vku.udn.vn>
2024-10-01 14:15:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+// Tran Quang -21IT303
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Xuc Xac'),
+          backgroundColor: Colors.red,
+        ),
+        body: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<MyApp> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset(
+                'assets/images/dice$leftDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+          //Get students to create the second die as a challenge
+          Expanded(
+            child: TextButton(
+              child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
